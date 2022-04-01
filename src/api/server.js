509d4c45ec8ce070b2c23d0f45480b18eaa31419
staaph <contact.staaph@gmail.com>
2022-04-01 03:18:34 +0200

    added functionality

--- src/api/server.js
+++ src/api/server.js
@@ -3,11 +3,11 @@ const axios = require('axios');
 
 const app = express();
 
-app.get('/api/ip/:query', async (req, res) => {
+app.get('/api/ip/:input', async (req, res) => {
   try {
-    const query = req.params.query;
+    const input = req.query;
     const data = await axios.get(
-      `https://geo.ipify.org/api/v2/country,city?apiKey=at_IHLnxkiPdY3h6J0XxspzeOFb2P614&ipAddress=${query}`
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_IHLnxkiPdY3h6J0XxspzeOFb2P614&ipAddress=${input}`
     );
     res.send(data.data);
   } catch (error) {
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,8 +18,14 @@
           />
         </div>
       </div>
+      <div
+        v-if="error"
+        class="bg-white text-bold text-red-600 rounded-lg mt-2 p-1 flex mx-auto justify-center"
+      >
+        Please provide valid id
+      </div>
       <!-- IP Info-->
-      <IPInfo v-if="ipInfo" v-bind:ipInfo="ipInfo" />
+      <IPInfo v-if="ipInfo && !error" v-bind:ipInfo="ipInfo" />
     </div>
 
     <!-- Map -->
@@ -27,65 +33,95 @@
   </div>
 </template>
 
-<script>
+<script setup>
 import IPInfo from '../components/IPInfo.vue';
 import ChevronRight from '../components/icons/ChevronRight.vue';
 import leaflet from 'leaflet';
 import { onMounted, ref } from 'vue';
 import axios from 'axios';
 
-export default {
-  components: { ChevronRight, IPInfo },
-  setup() {
-    let mymap;
+let mymap;
+let error = ref(null);
+let layerGroup = leaflet.layerGroup();
 
-    const queryIP = ref('');
-    const ipInfo = ref(null);
+const queryIP = ref('');
+const ipInfo = ref(null);
 
-    onMounted(() => {
-      mymap = leaflet
-        .map('mapid', { zoomControl: false })
-        .setView([51.505, -0.09], 9);
+// onMount get request user IP
+onMounted(async () => {
+  const data = await axios.get(
+    `https://geo.ipify.org/api/v2/country,city?apiKey=at_IHLnxkiPdY3h6J0XxspzeOFb2P614&ipAddress=`
+  );
 
-      leaflet
-        .tileLayer(
-          'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-          {
-            attribution:
-              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            maxZoom: 18,
-            id: 'mapbox/streets-v11',
-            tileSize: 512,
-            zoomOffset: -1,
-            accessToken:
-              'pk.eyJ1Ijoic3RhYXBoIiwiYSI6ImNsMWNuOTR1djAxMW8zY2xuaWZ5ZTN2ancifQ.WXEzWEp6e23j2LcWanOgkQ',
-          }
-        )
-        .addTo(mymap);
-    });
+  const result = data.data;
+  ipInfo.value = {
+    adress: result.ip,
+    state: result.location.region,
+    timezone: result.location.timezone,
+    isp: result.isp,
+    lat: result.location.lat,
+    lng: result.location.lng,
+  };
 
-    const getIpInfo = async () => {
-      try {
-        const data = await axios.get(
-          `https://geo.ipify.org/api/v2/country,city?apiKey=at_IHLnxkiPdY3h6J0XxspzeOFb2P614&ipAddress=${queryIP.value}`
-        );
+  mymap = leaflet
+    .map('mapid', { zoomControl: false })
+    .setView([ipInfo.value.lat, ipInfo.value.lng], 13);
 
-        const result = data.data;
-        ipInfo.value = {
-          adress: result.ip,
-          state: result.location.region,
-          timezone: result.location.timezone,
-          isp: result.isp,
-          lat: result.location.lat,
-          lng: result.location.lng,
-        };
-        leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]).addTo(mymap);
-        mymap.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
-      } catch (err) {
-        alert(err.message);
+  if (mymap.hasLayer(layerGroup)) {
+    layerGroup.clearLayers();
+  }
+
+  let marker = leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]);
+  layerGroup.addLayer(marker);
+  mymap.addLayer(layerGroup);
+
+  leaflet
+    .tileLayer(
+      'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          'pk.eyJ1Ijoic3RhYXBoIiwiYSI6ImNsMWNuOTR1djAxMW8zY2xuaWZ5ZTN2ancifQ.WXEzWEp6e23j2LcWanOgkQ',
       }
+    )
+    .addTo(mymap);
+});
+
+// request given IP on click, set error to true to show error
+const getIpInfo = async () => {
+  try {
+    const data = await axios.get(
+      `https://geo.ipify.org/api/v2/country,city?apiKey=at_IHLnxkiPdY3h6J0XxspzeOFb2P614&ipAddress=${queryIP.value}`
+    );
+
+    const result = data.data;
+    ipInfo.value = {
+      adress: result.ip,
+      state: result.location.region,
+      timezone: result.location.timezone,
+      isp: result.isp,
+      lat: result.location.lat,
+      lng: result.location.lng,
     };
-    return { queryIP, ipInfo, getIpInfo };
-  },
+
+    // mymap.removeLayer(marker);
+    if (mymap.hasLayer(layerGroup)) {
+      layerGroup.clearLayers();
+    }
+
+    let marker = leaflet.marker([ipInfo.value.lat, ipInfo.value.lng]);
+    layerGroup.addLayer(marker);
+    mymap.addLayer(layerGroup);
+
+    mymap.setView([ipInfo.value.lat, ipInfo.value.lng], 13);
+    error.value = false;
+  } catch (err) {
+    error.value = true;
+  }
 };
 </script>
